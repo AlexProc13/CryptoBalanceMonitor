9f57febb64a4388be9f4a4209b5fbc767c4ae33f
alexproc <alexproc1313@gmail.com>
2023-04-17 13:28:47 +0300

    simple test

--- tests/Feature/WalletTest.php
+++ tests/Feature/WalletTest.php
@@ -11,7 +11,7 @@ use App\Models\Wallet;
 
 class WalletTest extends TestCase
 {
-   // use RefreshDatabase;
+    use RefreshDatabase;
 
     public function setUp(): void
     {
@@ -41,10 +41,7 @@ class WalletTest extends TestCase
     {
         $count = mt_rand(5, 10);
         $wallets = Wallet::factory()->count($count)->create();
-
-dd(Wallet::all()->toArray());
         $response = $this->json(Request::METHOD_GET, route('wallets.index'));
-        dd($wallets->toArray(), route('wallets.index'));
         $response
             ->assertStatus(Response::HTTP_OK)
             ->assertJsonStructure([
@@ -53,10 +50,41 @@ dd(Wallet::all()->toArray());
                 'perPage',
                 'hasPages',
                 'previousPageUrl',
-                'data',
-                "previousPageUrl",
+                'previousPageUrl',
+                'data' => [
+                    [
+                        'id',
+                        'address',
+                        'balance',
+                        'createdAt',
+                        'balanceUpdated',
+                    ]
+                ],
             ])
-            ->assertJsonFragment(['status' => true, 'total' => $count])
+            ->assertJsonFragment(['status' => true])
+            ->assertJsonCount($wallets->count(), 'data')
             ->assertStatus(200);
+    }
+
+    public function testGetWalletStore(): void
+    {
+        //todo by facker
+        $addresses = [
+            'ltc1qzvcgmntglcuv4smv3lzj6k8szcvsrmvk0phrr9wfq8w493r096ssm2fgsw',
+            'ltc1qzvcgmntglcuv4smv3lzj6kfsdfsdfs8szcvsrmvk0phrr2fgsw'
+        ];
+        $data = ['currency' => 'LTC', 'address' => fake()->randomElement($addresses),];
+        $response = $this->json(Request::METHOD_POST, route('wallets.index'), $data);
+        $response
+            ->assertStatus(Response::HTTP_OK)
+            ->assertJsonStructure([
+                'status'
+            ])
+            ->assertJsonFragment(['status' => true])
+            ->assertStatus(200);
+
+        $this->assertDatabaseHas('wallets', [
+            'address' => $data['address'],
+        ]);
     }
 }
